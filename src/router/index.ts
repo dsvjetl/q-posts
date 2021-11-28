0cd5088a241c6dc91a6817a2862bd658f0fb5e1f
dsvjetl <domagoj.svjetlicic@gmail.com>
2021-11-28 22:58:35 +0100

    add home/posts route & lint code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,26 @@
 import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import PostsContainer from '@/views/PostsContainer.vue';
 
 Vue.use(VueRouter);
 
 const routes: RouteConfig[] = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+    {
+        path: '/',
+        name: 'posts',
+        component: PostsContainer,
+    },
+    // {
+    //     path: '/',
+    //     name: 'name',
+    //     component: () => import('../views/[container-name].vue'),
+    // },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes,
 });
 
 export default router;
---
+++ src/views/PostsContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="co-posts-container">
+        <p>Posts</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import { Component } from 'vue-property-decorator';
+
+    @Component({
+        name: 'PostsContainer',
+    })
+    export default class PostsContainer extends Vue {
+    }
+</script>
+
+<style lang="scss" scoped>
+    .co-posts-container {
+    }
+</style>
