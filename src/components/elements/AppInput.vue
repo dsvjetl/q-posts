c646b9652ef6c4188352e7644dc85f67ec094db6
dsvjetl <domagoj.svjetlicic@gmail.com>
2021-11-29 05:35:21 +0100

    implement filtering

---
+++ src/components/elements/AppInput.vue
@@ -0,0 +1,41 @@
+<template>
+    <input
+        type="text"
+        class="co-app-input h--15"
+        :placeholder="placeholder"
+        @keyup="onKeyup()"
+        v-model="inputText"
+    >
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component, Prop} from 'vue-property-decorator';
+
+    @Component({
+        name: 'AppInput',
+    })
+    export default class AppInput extends Vue {
+        @Prop({default: ''}) placeholder!: string;
+
+        inputText = '';
+
+        onKeyup() {
+            this.$emit('onKeyup', this.inputText);
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .co-app-input {
+        padding: 5px 10px;
+        color: #000;
+        border-radius: 3px;
+        border: 2px solid $green;
+
+        &:focus {
+            outline: none;
+            border: 2px solid $grey-light;
+        }
+    }
+</style>
--- src/interfaces/dto/PostDTO.ts
+++ src/interfaces/dto/PostDTO.ts
@@ -3,4 +3,5 @@ export interface PostDTO {
     id: number;
     title: string;
     completed: boolean;
+    body: string;
 }
